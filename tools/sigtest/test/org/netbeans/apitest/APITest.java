d48134c395d9e820dd20c2975c10169decd1cae7
Jaroslav Tulach <jst@seznam.cz>
2007-09-22 17:54:22 +0200

    Tests can be executed

---
+++ tools/sigtest/test/org/netbeans/apitest/APITest.java
@@ -0,0 +1,140 @@
+/*
+ * Copyright 2007 Sun Microsystems, Inc.  All Rights Reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.  Sun designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Sun in the LICENSE file that accompanied this code.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Sun Microsystems, Inc., 4150 Network Circle, Santa Clara,
+ * CA 95054 USA or visit www.sun.com if you need additional information or
+ * have any questions.
+ */
+package org.netbeans.apitest;
+
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.InputStream;
+import java.net.URL;
+import org.netbeans.junit.NbTestCase;
+
+/**
+ *
+ * @author Jaroslav Tulach
+ */
+public class APITest extends NbTestCase {
+    private static File workDir;
+    
+    public APITest(String s) {
+        super(s);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        clearWorkDir();
+        
+        
+    }
+    
+    
+    
+    public void testAntScript() throws Exception {
+        String c1 =
+            "package x;" +
+            "public class C {" +
+            "  private C() { }" +
+            "}";
+        String c2 =
+            "package x;" +
+            "public class C {" +
+            "  public C() { }" +
+            "}";
+        
+        compareAPIs(c1, c2);
+    }
+    
+    protected final void compareAPIs(String first, String second) throws Exception {
+        File d1 = new File(getWorkDir(), "d1");
+        File c1 = new File(d1, "C.java");
+        copy(first, c1);
+        
+        File d2 = new File(getWorkDir(), "d2");
+        File c2 = new File(d2, "C.java");
+        copy(first, c2);
+        
+        File build = new File(getWorkDir(), "build.xml");
+        extractResource("build.xml", build);
+        
+        String[] args = {
+            "-Ddir1=" + d1,
+            "-Ddir2=" + d2,
+        };
+        ExecuteUtils.execute(build, args);
+    }
+    
+    private static final void copy(String txt, File f) throws Exception {
+        f.getParentFile().mkdirs();
+        FileWriter w = new FileWriter(f);
+        w.append(txt);
+        w.close();
+    }
+
+    final File extractResource(String res, File f) throws Exception {
+        URL u = APITest.class.getResource(res);
+        assertNotNull ("Resource should be found " + res, u);
+        
+        FileOutputStream os = new FileOutputStream(f);
+        InputStream is = u.openStream();
+        for (;;) {
+            int ch = is.read ();
+            if (ch == -1) {
+                break;
+            }
+            os.write (ch);
+        }
+        os.close ();
+            
+        return f;
+    }
+    
+    final static String readFile (java.io.File f) throws java.io.IOException {
+        int s = (int)f.length ();
+        byte[] data = new byte[s];
+        assertEquals ("Read all data", s, new java.io.FileInputStream (f).read (data));
+        
+        return new String (data);
+    }
+    
+    final File extractString (String res, String nameExt) throws Exception {
+        File f = new File(getWorkDir(), nameExt);
+        f.deleteOnExit ();
+        
+        FileOutputStream os = new FileOutputStream(f);
+        InputStream is = new ByteArrayInputStream(res.getBytes("UTF-8"));
+        for (;;) {
+            int ch = is.read ();
+            if (ch == -1) {
+                break;
+            }
+            os.write (ch);
+        }
+        os.close ();
+            
+        return f;
+    }
+    
+}
---
+++ tools/sigtest/test/org/netbeans/apitest/ExecuteUtils.java
@@ -0,0 +1,254 @@
+/*
+ * Copyright 2007 Sun Microsystems, Inc.  All Rights Reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.  Sun designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Sun in the LICENSE file that accompanied this code.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Sun Microsystems, Inc., 4150 Network Circle, Santa Clara,
+ * CA 95054 USA or visit www.sun.com if you need additional information or
+ * have any questions.
+ */
+package org.netbeans.apitest;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileDescriptor;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.security.Permission;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import junit.framework.AssertionFailedError;
+import org.junit.Assert;
+
+/**
+ *
+ * @author Jaroslav Tulach
+ */
+final class ExecuteUtils {
+    
+    private ExecuteUtils() {
+    }
+/*    
+    final static void execute (String res, String[] args) throws Exception {
+        execute (extractResource (res), args);
+    }
+  */  
+    private static ByteArrayOutputStream out;
+    private static ByteArrayOutputStream err;
+    
+    final static String getStdOut() {
+        return out.toString();
+    }
+    final static String getStdErr() {
+        return err.toString();
+    }
+    
+    final static void execute(File f, String[] args) throws Exception {
+        // we need security manager to prevent System.exit
+        if (! (System.getSecurityManager () instanceof MySecMan)) {
+            out = new java.io.ByteArrayOutputStream ();
+            err = new java.io.ByteArrayOutputStream ();
+            System.setOut (new java.io.PrintStream (out));
+            System.setErr (new java.io.PrintStream (err));
+            
+            System.setSecurityManager (new MySecMan ());
+        }
+        
+        MySecMan sec = (MySecMan)System.getSecurityManager();
+        
+        // Jesse claims that this is not the right way how the execution
+        // of an ant script should be invoked:
+        //
+        // better IMHO to just run the task directly
+        // (setProject() and similar, configure its bean properties, and call
+        // execute()), or just make a new Project and initialize it.
+        // ant.Main.main is not intended for embedded use. Then you could get rid
+        // of the SecurityManager stuff, would be cleaner I think.
+        //
+        // If I had to write this once again, I would try to follow the
+        // "just make a new Project and initialize it", but as this works
+        // for me now, I leave it for the time when somebody really
+        // needs that...
+        
+        List<String> arr = new ArrayList<String>();
+        arr.add ("-f");
+        arr.add (f.toString ());
+        arr.addAll(Arrays.asList(args));
+        
+        
+        out.reset ();
+        err.reset ();
+        
+        try {
+            sec.setActive(true);
+            org.apache.tools.ant.Main.main (arr.toArray(new String[0]));
+        } catch (MySecExc ex) {
+            Assert.assertNotNull ("The only one to throw security exception is MySecMan and should set exitCode", sec.exitCode);
+            ExecutionError.assertExitCode (
+                "Execution has to finish without problems",
+                sec.exitCode.intValue ()
+            );
+        } finally {
+            sec.setActive(false);
+        }
+    }
+    
+    static class ExecutionError extends AssertionFailedError {
+        public final int exitCode;
+        
+        public ExecutionError (String msg, int e) {
+            super (msg);
+            this.exitCode = e;
+        }
+        
+        public static void assertExitCode (String msg, int e) {
+            if (e != 0) {
+                throw new ExecutionError (
+                    msg + " was: " + e + "\nOutput: " + out.toString () +
+                    "\nError: " + err.toString (),
+                    e
+                );
+            }
+        }
+    }
+    
+    private static class MySecExc extends SecurityException {
+        @Override
+        public void printStackTrace() {
+        }
+        @Override
+        public void printStackTrace(PrintStream ps) {
+        }
+        public void printStackTrace(PrintWriter ps) {
+        }
+    }
+    
+    private static class MySecMan extends SecurityManager {
+        public Integer exitCode;
+        
+        private boolean active;
+        
+        public void checkExit (int status) {
+            if (active) {
+                exitCode = new Integer (status);
+                throw new MySecExc ();
+            }
+        }
+
+        public void checkPermission(Permission perm, Object context) {
+        }
+
+        public void checkPermission(Permission perm) {
+        /*
+            if (perm instanceof RuntimePermission) {
+                if (perm.getName ().equals ("setIO")) {
+                    throw new MySecExc ();
+                }
+            }
+         */
+        }
+
+        public void checkMulticast(InetAddress maddr) {
+        }
+
+        public void checkAccess (ThreadGroup g) {
+        }
+
+        public void checkWrite (String file) {
+        }
+
+        public void checkLink (String lib) {
+        }
+
+        public void checkExec (String cmd) {
+        }
+
+        public void checkDelete (String file) {
+        }
+
+        public void checkPackageAccess (String pkg) {
+        }
+
+        public void checkPackageDefinition (String pkg) {
+        }
+
+        public void checkPropertyAccess (String key) {
+        }
+
+        public void checkRead (String file) {
+        }
+
+        public void checkSecurityAccess (String target) {
+        }
+
+        public void checkWrite(FileDescriptor fd) {
+        }
+
+        public void checkListen (int port) {
+        }
+
+        public void checkRead(FileDescriptor fd) {
+        }
+
+        public void checkMulticast(InetAddress maddr, byte ttl) {
+        }
+
+        public void checkAccess (Thread t) {
+        }
+
+        public void checkConnect (String host, int port, Object context) {
+        }
+
+        public void checkRead (String file, Object context) {
+        }
+
+        public void checkConnect (String host, int port) {
+        }
+
+        public void checkAccept (String host, int port) {
+        }
+
+        public void checkMemberAccess (Class clazz, int which) {
+        }
+
+        public void checkSystemClipboardAccess () {
+        }
+
+        public void checkSetFactory () {
+        }
+
+        public void checkCreateClassLoader () {
+        }
+
+        public void checkAwtEventQueueAccess () {
+        }
+
+        public void checkPrintJobAccess () {
+        }
+
+        public void checkPropertiesAccess () {
+        }
+
+        void setActive(boolean b) {
+            active = b;
+        }
+    } // end of MySecMan
+
+}
