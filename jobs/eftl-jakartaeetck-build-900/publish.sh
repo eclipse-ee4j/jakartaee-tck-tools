f7d44681bcaa1f664e15e655fabbe45104b170e0
David Blevins <david.blevins@gmail.com>
2020-08-14 16:45:03 -0700

    Exact copy of the bash from https://ci.eclipse.org/jakartaee-tck/job/eftl-jakartaeetck-build-900/configure
    Signed-off-by: David Blevins <david.blevins@gmail.com>

---
+++ jobs/eftl-jakartaeetck-build-900/publish.sh
@@ -0,0 +1,43 @@
+#!/bin/bash -x
+
+#GF_VER=`cat glassfish.version | tr '\n' '|'`
+TCK_VER=`cat jakartaeetck.version | tr '\n' '|'`
+echo "Version Info:Eclipse GF URL : https://ci.eclipse.org/jakartaee-tck/job/build-glassfish/lastSuccessfulBuild/artifact/appserver/distributions/glassfish/target/glassfish.zip ; JakartaEETCK-$TCK_VER"
+
+for f in *junitreports.tar.gz
+do
+  tar zxf "$f" -C $WORKSPACE  --strip-components=5
+  touch */*.xml
+done
+
+ls -ltr
+cd jakartaeetck-bundles/
+ls -ltr
+
+rm -rf $WORKSPACE/jakarta-jakartaeetckinfo.txt
+touch $WORKSPACE/jakarta-jakartaeetckinfo.txt
+export BUNDLE_URL="http://download.eclipse.org/ee4j/jakartaee-tck/jakartaee9-eftl/staged-900"
+
+for tck_bundle in *tck*.zip
+do
+  export NAME=$tck_bundle
+  echo '***********************************************************************************' >> $WORKSPACE/jakarta-jakartaeetckinfo.txt
+  echo '***                        TCK bundle information                               ***' >> $WORKSPACE/jakarta-jakartaeetckinfo.txt
+  echo "*** Name:       ${NAME}                                     ***" >> $WORKSPACE/jakarta-jakartaeetckinfo.txt
+  echo "*** Bundle Copied to URL:    ${BUNDLE_URL}/${NAME} ***"  >> $WORKSPACE/jakarta-jakartaeetckinfo.txt
+  echo '*** Date and size: '`stat -c "date: %y, size(b): %s" ${NAME}`'        ***'>> $WORKSPACE/jakarta-jakartaeetckinfo.txt
+  echo "*** SHA256SUM: "`sha256sum ${NAME} | awk '{print $1}'`' ***' >> $WORKSPACE/jakarta-jakartaeetckinfo.txt
+  echo '***                                                                             ***' >> $WORKSPACE/jakarta-jakartaeetckinfo.txt
+  echo '***********************************************************************************' >> $WORKSPACE/jakarta-jakartaeetckinfo.txt
+done
+
+
+scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null jakarta-jakartaeetck-9.0.0.zip genie.jakartaee-tck@build.eclipse.org:/home/data/httpd/download.eclipse.org/ee4j/jakartaee-tck/jakartaee9-eftl/staged-900
+scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null $WORKSPACE/jakartaeetck.version genie.jakartaee-tck@build.eclipse.org:/home/data/httpd/download.eclipse.org/ee4j/jakartaee-tck/jakartaee9-eftl/staged-900
+scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null $WORKSPACE/jakarta-jakartaeetckinfo.txt genie.jakartaee-tck@build.eclipse.org:/home/data/httpd/download.eclipse.org/ee4j/jakartaee-tck/jakartaee9-eftl/staged-900
+
+rm -rf jakarta-jakartaeetck-9.0.0.zip
+rm -rf jakarta-jakartaee-smoke-9.0.0.zip
+
+ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null genie.jakartaee-tck@build.eclipse.org ls -lt /home/data/httpd/download.eclipse.org/ee4j/jakartaee-tck/jakartaee9-eftl/staged-900
+
