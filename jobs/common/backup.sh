454e524a49098d79085051418bc619685e51b476
Scott Marlow <smarlow@redhat.com>
2020-10-15 19:02:17 -0400

    temporarily remove the max number of backups
    Signed-off-by: Scott Marlow <smarlow@redhat.com>

--- jobs/common/backup.sh
+++ jobs/common/backup.sh
@@ -32,3 +32,20 @@ function backup {
     # Upload the latest binary with correct date and sha
     scp $SSH_OPTS "$FILE" "$HOST:$DEST/$NAME-$DATE-$SHA.zip"
 }
+
+function backup_no_delete {
+    FILE="${1?Specify a binary}"
+    HOST="${2?Specify a host}"
+    DEST="${3?Specify a destination directory in $HOST}"
+
+    #################################################
+
+    NAME="${FILE/.zip/}"
+    SHA=$(sha256sum "$FILE" | sed 's, .*,,')
+    DATE="$(date +"%Y-%m-%d")"
+
+    SSH_OPTS="-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"
+
+    # Upload the latest binary with correct date and sha
+    scp $SSH_OPTS "$FILE" "$HOST:$DEST/$NAME-$DATE-$SHA.zip"
+}
