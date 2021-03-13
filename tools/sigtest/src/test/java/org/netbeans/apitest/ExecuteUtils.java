377c2e42389ad3c93966111a119352099ecfc757
Jaroslav Tulach <jaroslav.tulach@apidesign.org>
2021-03-13 17:11:59 +0100

    Fallback to (slower) external javac on JDK 1.8 to avoid LinkageErrors

--- tools/sigtest/src/test/java/org/netbeans/apitest/ExecuteUtils.java
+++ tools/sigtest/src/test/java/org/netbeans/apitest/ExecuteUtils.java
@@ -34,32 +34,27 @@ import java.security.Permission;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import junit.framework.Assert;
 import junit.framework.AssertionFailedError;
+import org.junit.Assert;
 
 /**
  *
  * @author Jaroslav Tulach
  */
 final class ExecuteUtils {
-    
-    private ExecuteUtils() {
-    }
-/*    
-    final static void execute (String res, String[] args) throws Exception {
-        execute (extractResource (res), args);
-    }
-  */  
     private static ByteArrayOutputStream out;
     private static ByteArrayOutputStream err;
-    
+
+    private ExecuteUtils() {
+    }
+
     final static String getStdOut() {
         return out.toString();
     }
     final static String getStdErr() {
         return err.toString();
     }
-    
+
     final static void execute(File f, String[] args) throws Exception {
         // we need security manager to prevent System.exit
         if (! (System.getSecurityManager () instanceof MySecMan)) {
@@ -67,12 +62,12 @@ final class ExecuteUtils {
             err = new java.io.ByteArrayOutputStream ();
             System.setOut (new java.io.PrintStream (out));
             System.setErr (new java.io.PrintStream (err));
-            
+
             System.setSecurityManager (new MySecMan ());
         }
-        
+
         MySecMan sec = (MySecMan)System.getSecurityManager();
-        
+
         // Jesse claims that this is not the right way how the execution
         // of an ant script should be invoked:
         //
@@ -86,39 +81,38 @@ final class ExecuteUtils {
         // "just make a new Project and initialize it", but as this works
         // for me now, I leave it for the time when somebody really
         // needs that...
-        
-        List<String> arr = new ArrayList<String>();
+
+        List<String> arr = new ArrayList<>();
         arr.add ("-f");
         arr.add (f.toString ());
         arr.addAll(Arrays.asList(args));
         arr.add ("-verbose");
-        
-        
+        if (System.getProperty("java.version").startsWith("1.8")) {
+            arr.add ("-Dbuild.compiler=extjavac");
+        }
+
         out.reset ();
         err.reset ();
-        
+
         try {
             sec.setActive(true);
             org.apache.tools.ant.Main.main (arr.toArray(new String[0]));
         } catch (MySecExc ex) {
             Assert.assertNotNull ("The only one to throw security exception is MySecMan and should set exitCode", sec.exitCode);
-            ExecutionError.assertExitCode (
-                "Execution has to finish without problems",
-                sec.exitCode.intValue ()
-            );
+            ExecutionError.assertExitCode ("Execution has to finish without problems", sec.exitCode);
         } finally {
             sec.setActive(false);
         }
     }
-    
+
     static class ExecutionError extends AssertionFailedError {
         public final int exitCode;
-        
+
         public ExecutionError (String msg, int e) {
             super (msg);
             this.exitCode = e;
         }
-        
+
         public static void assertExitCode (String msg, int e) {
             if (e != 0) {
                 throw new ExecutionError (
@@ -129,7 +123,7 @@ final class ExecuteUtils {
             }
         }
     }
-    
+
     private static class MySecExc extends SecurityException {
         @Override
         public void printStackTrace() {
@@ -137,25 +131,29 @@ final class ExecuteUtils {
         @Override
         public void printStackTrace(PrintStream ps) {
         }
+        @Override
         public void printStackTrace(PrintWriter ps) {
         }
     }
-    
+
     private static class MySecMan extends SecurityManager {
         public Integer exitCode;
-        
+
         private boolean active;
-        
+
+        @Override
         public void checkExit (int status) {
             if (active) {
-                exitCode = new Integer (status);
+                exitCode = status;
                 throw new MySecExc ();
             }
         }
 
+        @Override
         public void checkPermission(Permission perm, Object context) {
         }
 
+        @Override
         public void checkPermission(Permission perm) {
         /*
             if (perm instanceof RuntimePermission) {
@@ -166,84 +164,115 @@ final class ExecuteUtils {
          */
         }
 
+        @Override
         public void checkMulticast(InetAddress maddr) {
         }
 
+        @Override
         public void checkAccess (ThreadGroup g) {
         }
 
+        @Override
         public void checkWrite (String file) {
         }
 
+        @Override
         public void checkLink (String lib) {
         }
 
+        @Override
         public void checkExec (String cmd) {
         }
 
+        @Override
         public void checkDelete (String file) {
         }
 
+        @Override
         public void checkPackageAccess (String pkg) {
         }
 
+        @Override
         public void checkPackageDefinition (String pkg) {
         }
 
+        @Override
         public void checkPropertyAccess (String key) {
         }
 
+        @Override
         public void checkRead (String file) {
         }
 
+        @Override
         public void checkSecurityAccess (String target) {
         }
 
+        @Override
         public void checkWrite(FileDescriptor fd) {
         }
 
+        @Override
         public void checkListen (int port) {
         }
 
+        @Override
         public void checkRead(FileDescriptor fd) {
         }
 
+        @Override
+        @SuppressWarnings("deprecation")
         public void checkMulticast(InetAddress maddr, byte ttl) {
         }
 
+        @Override
         public void checkAccess (Thread t) {
         }
 
+        @Override
         public void checkConnect (String host, int port, Object context) {
         }
 
+        @Override
         public void checkRead (String file, Object context) {
         }
 
+        @Override
         public void checkConnect (String host, int port) {
         }
 
+        @Override
         public void checkAccept (String host, int port) {
         }
 
+        @Override
+        @SuppressWarnings("deprecation")
         public void checkMemberAccess (Class clazz, int which) {
         }
 
+        @Override
+        @SuppressWarnings("deprecation")
         public void checkSystemClipboardAccess () {
         }
 
+        @Override
         public void checkSetFactory () {
         }
 
+        @Override
         public void checkCreateClassLoader () {
         }
 
+        @Override
+        @SuppressWarnings("deprecation")
         public void checkAwtEventQueueAccess () {
         }
 
+        @Override
         public void checkPrintJobAccess () {
         }
 
+        @Override
         public void checkPropertiesAccess () {
         }
 
