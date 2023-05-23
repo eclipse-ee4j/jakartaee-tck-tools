0ed54a6fe90155d6ff678a8dc7d40f72a4a98aa2
Scott Marlow <smarlow@redhat.com>
2023-05-23 11:04:43 -0400

    Add war artifact getters access to JarProcessor for Jar2ShrinkWrap
    Signed-off-by: Scott Marlow <smarlow@redhat.com>

--- tools/jar2shrinkwrap/src/main/java/jakartatck/jar2shrinkwrap/JarProcessor.java
+++ tools/jar2shrinkwrap/src/main/java/jakartatck/jar2shrinkwrap/JarProcessor.java
@@ -1,6 +1,7 @@
 package jakartatck.jar2shrinkwrap;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.zip.ZipEntry;
 
 /**
@@ -13,4 +14,15 @@ public interface JarProcessor {
     void process(final ZipEntry entry);
 
     void saveOutput(final File FileInputArchive);
+
+    public ArrayList<String> getLibraries();
+
+    public ArrayList<String> getMetainf();
+
+    public ArrayList<String> getWebinf();
+
+    public ArrayList<String> getClasses();
+
+    public ArrayList<String> getOtherFiles();
+
 }
