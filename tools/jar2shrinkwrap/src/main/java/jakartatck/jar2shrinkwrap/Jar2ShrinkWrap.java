ec53957286823546180923f0671728e4dcfb3947
Scott Marlow <smarlow@redhat.com>
2023-05-23 14:47:21 -0400

    download zip and extract contents
    Signed-off-by: Scott Marlow <smarlow@redhat.com>

--- tools/jar2shrinkwrap/src/main/java/jakartatck/jar2shrinkwrap/Jar2ShrinkWrap.java
+++ tools/jar2shrinkwrap/src/main/java/jakartatck/jar2shrinkwrap/Jar2ShrinkWrap.java
@@ -1,9 +1,15 @@
 package jakartatck.jar2shrinkwrap;
 
-import java.io.BufferedReader;
+import java.io.BufferedInputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 
 /**
  * Jar2ShrinkWrap
@@ -12,12 +18,22 @@ import java.io.InputStreamReader;
  */
 public class Jar2ShrinkWrap {
 
-    private static String LegacyTCKFolderPropName = "LegacyTCKFolder";
-    private static String defaultFolderName = "legacytck";
-    private static String legacyTCKZip = "jakarta-jakartaeetck-10.0.2.zip";
-    private static String legacyTCKZipDownload = "https://download.eclipse.org/jakartaee/platform/10/jakarta-jakartaeetck-10.0.2.zip";
-    private static String legacyTCKFolder = "jakartaeetck";
-    private static String LegacyTCKFolderName = System.getProperty(LegacyTCKFolderPropName,System.getProperty("java.io.tmpdir") + File.separator + defaultFolderName);
+    private static final String LegacyTCKFolderPropName = "LegacyTCKFolder";
+    private static final String defaultFolderName = "legacytck";
+    private static final String legacyTCKZip = "jakarta-jakartaeetck-10.0.2.zip";
+    private static final URL tckurl;
+
+    static {
+        try {
+            tckurl = new URL("https://download.eclipse.org/jakartaee/platform/10/jakarta-jakartaeetck-10.0.2.zip");
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static final String legacyTCKZipDownload = "https://download.eclipse.org/jakartaee/platform/10/jakarta-jakartaeetck-10.0.2.zip";
+    private static final String legacyTCKFolder = "jakartaeetck";
+    private static String LegacyTCKFolderName = System.getProperty(LegacyTCKFolderPropName, System.getProperty("java.io.tmpdir") + File.separator + defaultFolderName);
 
     public static JarProcessor fromPackage(String packageName) {
         System.out.println("looking for existing copy of jakarta-jakartaeetck-10.0.2.zip in folder " + LegacyTCKFolderName);
@@ -27,20 +43,22 @@ public class Jar2ShrinkWrap {
         }
         if (LegacyTCKFolderName == null) {
             LegacyTCKFolderName = System.getProperty("java.io.tmpdir") + File.separator + defaultFolderName;
-            System.out.println( LegacyTCKFolderPropName + "wasn't specified so will instead use " + LegacyTCKFolderName);
+            System.out.println(LegacyTCKFolderPropName + "wasn't specified so will instead use " + LegacyTCKFolderName);
         }
         File legacyTCKFolder = new File(LegacyTCKFolderName);
         legacyTCKFolder.mkdirs();
-        System.out.println("looking for existing extracted " + legacyTCKZip + " in folder " + LegacyTCKFolderName );
+        System.out.println("looking for existing extracted " + legacyTCKZip + " in folder " + LegacyTCKFolderName);
 
-        File target = new File(legacyTCKFolder,"LegacyTCKFolderName");
-        if(target.exists()) {
-            System.out.println("reusing existing legacy TCK folder for reading TCK archives" + target.getName());
+        File target = new File(legacyTCKFolder, "LegacyTCKFolderName");
+        target.mkdirs();
+        File targetTCKZipFile = new File(target, legacyTCKZip);
+        if (targetTCKZipFile.exists()) {
+            System.out.println("already downloaded " + targetTCKZipFile.getName());
         } else {
             System.out.println("will download " + legacyTCKZipDownload + " and extract contents into " + target.getName());
-            downloadLegacyTCK(target);
+            downloadUsingStream(tckurl, target);
 
-            System.out.println("will unzip " + legacyTCKZipDownload + " into " +target.getName());
+            System.out.println("will unzip " + legacyTCKZipDownload + " into " + target.getName());
             unzip(target);
             System.out.println("one time setup is complete");
         }
@@ -55,7 +73,7 @@ public class Jar2ShrinkWrap {
     }
 
     private static File locateTargetPackageFolder(File target, String packageName) {
-        File findTCKDistArchive = new File(target,"dist" + package2Name(packageName));
+        File findTCKDistArchive = new File(target, "dist" + package2Name(packageName));
         System.out.println("looking inside of " + findTCKDistArchive.getName() + " for the archive that contains a test client for package " + packageName);
         // TODO: Add support for ear achives which can contain jar + war files
         // TODO: Add support for jar archives
@@ -63,7 +81,7 @@ public class Jar2ShrinkWrap {
         if (findTCKDistArchive.exists()) {
             File[] possibleMatches = findTCKDistArchive.listFiles();
             File match = null;
-            for (File targetWarFile: possibleMatches) {
+            for (File targetWarFile : possibleMatches) {
                 if (targetWarFile.getName().endsWith(".war")) {
                     if (match != null) {
                         throw new RuntimeException("found multiple matches for " + packageName + " " + match.getName() + " vs " + targetWarFile.getName());
@@ -84,32 +102,69 @@ public class Jar2ShrinkWrap {
         return packageName.replace(".", File.pathSeparator);
     }
 
-    private static void unzip(File target) {
-    }
-
-    private static void downloadLegacyTCK(File targetFolder) {
-
-        ProcessBuilder processBuilder = new ProcessBuilder();
-        processBuilder.command("bash", "-c", "wget", "--directory-prefix=" + targetFolder.getName());
+    private static void unzip(File fileFolder) {
+        File file = new File(fileFolder, legacyTCKZip);
+        byte[] buffer = new byte[10240];
+        System.out.println("Unzipping " + file.getName());
         try {
-            Process process = processBuilder.start();
-            StringBuilder output = new StringBuilder();
-            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-            String line;
-            while ((line = reader.readLine()) != null) {
-            output.append(line + "\n");
+            ZipInputStream zipInputStream = new ZipInputStream(new FileInputStream(file));
+            ZipEntry zipEntry = zipInputStream.getNextEntry();
+            while (zipEntry != null) {
+                File newFile = new File(fileFolder, zipEntry.getName());
+                String destDirPath = fileFolder.getCanonicalPath();
+                String destFilePath = newFile.getCanonicalPath();
+                if (!destFilePath.startsWith(destDirPath + File.separator)) {
+                    throw new IOException("Entry is outside of the target dir: " + zipEntry.getName());
+                }
+                if (zipEntry.isDirectory()) {
+                    if (!newFile.isDirectory() && !newFile.mkdirs()) {
+                        throw new IOException("Could not create folder " + newFile.getName());
+                    }
+                } else {
+                    if (!newFile.getParentFile().isDirectory() && !newFile.getParentFile().mkdirs()) {
+                        throw new IOException("Could not create parent folder for " + newFile.getName());
+                    }
+                    FileOutputStream fileOutputStream = new FileOutputStream(newFile);
+                    int len;
+                    while ((len = zipInputStream.read(buffer)) > 0) {
+                        fileOutputStream.write(buffer, 0, len);
+                    }
+                    fileOutputStream.close();
+                }
+                zipEntry = zipInputStream.getNextEntry();
             }
-            int exitVal = process.waitFor();
-            if (exitVal != 0) {
-                System.out.println("failure calling wget (for " + targetFolder + ") , Calling exit with code 1.");
-                System.exit(1);
-            }
-            reader.close();
+            zipInputStream.closeEntry();
+            zipInputStream.close();
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
         } catch (IOException e) {
             throw new RuntimeException(e);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
         }
-
+        System.out.println("Finished Unzipping " + file.getName());
     }
+
+    private static void downloadUsingStream(URL url, File fileFolder) {
+        try {
+            File file = new File(fileFolder, legacyTCKZip);
+            BufferedInputStream bis = new BufferedInputStream(url.openStream());
+            FileOutputStream fis = new FileOutputStream(file);
+            byte[] buffer = new byte[10240];
+            int count = 0;
+            int loop = 0;
+            System.out.println("downloading from " + url + " to " + fileFolder.getName());
+            while ((count = bis.read(buffer, 0, 10240)) != -1) {
+                fis.write(buffer, 0, count);
+                if (loop++ == 1024 ) {
+                    System.out.print(".");
+                    loop = 0;
+                }
+            }
+            fis.close();
+            bis.close();
+            System.out.println("finished download of " + url);
+        } catch (Throwable e) {
+            throw new RuntimeException(e.getMessage(), e);
+        }
+    }
+
 }
--- tools/jar2shrinkwrap/src/main/java/jakartatck/jar2shrinkwrap/Main.java
+++ tools/jar2shrinkwrap/src/main/java/jakartatck/jar2shrinkwrap/Main.java
@@ -12,16 +12,16 @@ public class Main {
     public static void main(String[] args) {
         for (String file : args) {
             System.out.println("process input file " + file);
-            if(file.startsWith("com.sun.ts.tests")) {
+            if (file.startsWith("com.sun.ts.tests")) {
                 System.out.println("looking for TCK test package " + file);
-                JarProcessor jarProcessor =  Jar2ShrinkWrap.fromPackage(file);
+                JarProcessor jarProcessor = Jar2ShrinkWrap.fromPackage(file);
                 System.out.println("jarProcessor classes " + jarProcessor.getClasses());
                 System.out.println("jarProcessor libraries " + jarProcessor.getLibraries());
                 System.out.println("jarProcessor metainf " + jarProcessor.getMetainf());
                 System.out.println("jarProcessor webinf " + jarProcessor.getWebinf());
                 System.out.println("jarProcessor other files " + jarProcessor.getOtherFiles());
                 System.out.println("done with TCK test package " + file);
-                System.out.println("");
+                System.out.println();
             }
             File fileInputArchive = new File(file);
             JarVisit visitor = new JarVisit(fileInputArchive);
