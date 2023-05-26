08224cdc3b0010f8fb9146ff590b7860a88022dc
Scott M Stark <starksm64@gmail.com>
2023-05-25 22:58:16 -0600

    Changes to produce proper WebArchive @Deployment code
    Signed-off-by: Scott M Stark <starksm64@gmail.com>

--- tools/jar2shrinkwrap/src/main/java/jakartatck/jar2shrinkwrap/JarProcessor.java
+++ tools/jar2shrinkwrap/src/main/java/jakartatck/jar2shrinkwrap/JarProcessor.java
@@ -1,8 +1,10 @@
 package jakartatck.jar2shrinkwrap;
 
 import java.io.File;
+import java.io.Writer;
 import java.util.ArrayList;
 import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 
 /**
  * JarProcessor
@@ -11,11 +13,20 @@ import java.util.zip.ZipEntry;
  */
 public interface JarProcessor {
 
-    void process(final ZipEntry entry);
+    void process(final ZipInputStream zipInputStream, final ZipEntry entry);
+
+    /**
+     * Write the Arquillian @Deployment method java code into the given StringWriter.
+     * @param writer - writer to use for the deployment method java source text
+     * @param includeImports - flag to indicate whether the arquillian imports should be added to the
+     *                       source output
+     */
+    void saveOutput(Writer writer, boolean includeImports);
 
     void saveOutput(final File FileInputArchive);
 
     ArrayList<String> getLibraries();
+    File getLibDir();
 
     ArrayList<String> getMetainf();
 
