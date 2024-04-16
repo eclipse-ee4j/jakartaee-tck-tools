24a15f6ad8e6c242e0513d31c7ce217484d4d469
Scott M Stark <starksm64@gmail.com>
2024-04-16 11:03:25 -0600

    - Update the com.sun.tdk.signaturetest.Version class to get version info from the artifact jar manifest. - Remove the obsolete release profile used in jboss fork Fixes #33
    Signed-off-by: Scott M Stark <starksm64@gmail.com>

--- tools/sigtest/src/main/java/com/sun/tdk/signaturetest/Version.java
+++ tools/sigtest/src/main/java/com/sun/tdk/signaturetest/Version.java
@@ -29,16 +29,82 @@ package com.sun.tdk.signaturetest;
 
 import com.sun.tdk.signaturetest.util.I18NResourceBundle;
 
+import java.net.JarURLConnection;
+import java.net.URL;
+import java.security.CodeSource;
+import java.security.ProtectionDomain;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.jar.Attributes;
+import java.util.jar.Manifest;
+
 public class Version {
 
     private static I18NResourceBundle i18n =
             I18NResourceBundle.getBundleForClass(Version.class);
 
-    // the following constatnts should be filled in by the build script
-    public static final String Number="2.2";
-    public static final String build_time="";
-    public static final String build_os="";
-    public static final String build_user="";
+    // the following constants should be filled in by the build script
+    public static final String Number;
+    public static final String build_time;
+    public static final String build_os;
+    public static final String build_user;
+
+    public static final String IMPLEMENTATION_BUILD_OS = "Implementation-Build-OS";
+
+    public static final String IMPLEMENTATION_BUILD_TIME = "Implementation-Build-Time";
+
+    static {
+        // Defaults
+        String number = "Unknown";
+        String buildOS = System.getProperty("os.name") + " " + System.getProperty("os.version");
+        String buildUser = System.getProperty("user.name");
+        ZonedDateTime gmt = ZonedDateTime.now(ZoneId.of("GMT"));
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss'Z'");
+        String buildTime = gmt.format(formatter);
+        // Look for build info from the jar manifest
+        try {
+            // Get the URL of the jar file from the CodeSource
+            ProtectionDomain pd = Version.class.getProtectionDomain();
+            CodeSource cs = pd.getCodeSource();
+            URL url = cs.getLocation();
+            // Open a JarURLConnection to the jar file's URL
+            if(url.getProtocol().equals("file")) {
+                // If the URL is a file URL, convert it to a jar URL if it is actually a jar file
+                String path = url.getPath();
+                if(path.endsWith(".jar")) {
+                    url = new URL("jar:file:" + path + "!/");
+                } else {
+                    throw new IllegalStateException(String.format("Warning: %s is not a jar file\n", path));
+                }
+            }
+            JarURLConnection jarConnection = (JarURLConnection) url.openConnection();
+            // Get the Manifest from the JarURLConnection
+            Manifest mf = jarConnection.getManifest();
+            // Read the attributes from the Manifest
+            Attributes attrs = mf.getMainAttributes();
+            if(attrs.containsKey(Attributes.Name.IMPLEMENTATION_VERSION)) {
+                number = attrs.getValue(Attributes.Name.IMPLEMENTATION_VERSION);
+            }
+            if(attrs.containsKey(Attributes.Name.IMPLEMENTATION_VENDOR)) {
+                buildUser = attrs.getValue(Attributes.Name.IMPLEMENTATION_VENDOR);
+            }
+            if(attrs.containsKey(IMPLEMENTATION_BUILD_OS)) {
+                buildOS = attrs.getValue(IMPLEMENTATION_BUILD_OS);
+            }
+            if(attrs.containsKey(IMPLEMENTATION_BUILD_TIME)) {
+                buildTime = attrs.getValue(IMPLEMENTATION_BUILD_TIME);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            // Set the values read from manifest or the defaults
+            Number = number;
+            build_os = buildOS;
+            build_time = buildTime;
+            build_user = buildUser;
+        }
+    }
 
     public static String getVersionInfo() {
         StringBuffer sb = new StringBuffer();
