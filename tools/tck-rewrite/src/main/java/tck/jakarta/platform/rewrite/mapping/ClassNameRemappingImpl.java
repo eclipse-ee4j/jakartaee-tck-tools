e26ab3433ab569daaf732137ed2fbc416bde07bc
Scott Marlow <smarlow@redhat.com>
2024-06-10 16:41:46 -0400

    do not add $.class to shrinkwrap and remove tck-rewrite code that has no impact
    Signed-off-by: Scott Marlow <smarlow@redhat.com>

--- tools/tck-rewrite/src/main/java/tck/jakarta/platform/rewrite/mapping/ClassNameRemappingImpl.java
+++ tools/tck-rewrite/src/main/java/tck/jakarta/platform/rewrite/mapping/ClassNameRemappingImpl.java
@@ -21,37 +21,26 @@ public class ClassNameRemappingImpl implements ClassNameRemapping {
         this.getClassBeingUpdatedNoClass = classBeingUpdated.substring(0, classBeingUpdated.length() - CLASS.length());
     }
 
+    /**
+     *
+     * @param className is an EE 10 TCK test class name
+     * @return is an EE 11 TCK test class name
+     */
     public String getName(String className) {
 
         className = className.replace("com.sun.ts.tests.jpa.common.schema30", "ee.jakarta.tck.persistence.common.schema30").
-                replace("com.sun.ts.tests.jpa", "ee.jakarta.tck.persistence").
-                replace("ee.jakarta.tck.persistence.core.query.language.Client.class", classBeingUpdated).
-                replace("ee.jakarta.tck.persistence.core.criteriaapi.misc.Client.class", classBeingUpdated).
-                replace("ee.jakarta.tck.persistence.core.criteriaapi.From.Client.class", classBeingUpdated).
-                replace("ee.jakarta.tck.persistence.core.criteriaapi.metamodelquery.Client.class", classBeingUpdated).
-                replace("ee.jakarta.tck.persistence.core.criteriaapi.CriteriaBuilder.Client.class", classBeingUpdated).
-                replace("ee.jakarta.tck.persistence.core.criteriaapi.CriteriaQuery.Client.class", classBeingUpdated).
-                replace("ee.jakarta.tck.persistence.core.annotations.orderby.Client.class", classBeingUpdated).
-                replace("ee.jakarta.tck.persistence.core.entityManager2.Client.class", classBeingUpdated).
-                replace("ee.jakarta.tck.persistence.core.entityManager.Client.class", classBeingUpdated).
-                replace("ee.jakarta.tck.persistence.core.query.apitests.Client.class", classBeingUpdated).
-                replace("ee.jakarta.tck.persistence.core.criteriaapi.Join.Client.class", classBeingUpdated).
-                replace("ee.jakarta.tck.persistence.core.types.property.Client.class", classBeingUpdated)
-                ;
-        if (getClassBeingUpdatedNoClass.equals("ee.jakarta.tck.persistence.core.criteriaapi.CriteriaQuery.Client3")) {
-            className = className.replace("ee.jakarta.tck.persistence.core.criteriaapi.CriteriaQuery.Client$ExpectedResult.class", getClassBeingUpdatedNoClass + "$ExpectedResult.class");
-        } else if (getClassBeingUpdatedNoClass.startsWith("ee.jakarta.tck.persistence.core.criteriaapi.CriteriaQuery.Client")) {
-
-        }
-
-
-
+                replace("com.sun.ts.tests.jpa", "ee.jakarta.tck.persistence");
+        // TODO: also handle ee.jakarta.tck.pages
         return className;
 
     }
 
     @Override
     public boolean shouldBeIgnored(String className) {
+        if(className.contains("$")) { // ignore Client$ExpectedResult
+            return true;
+        }
+
         // do not put TCK vehicle classes in the (ShrinkWrap) test deployment archives
         //if(className != null && className.startsWith("com.sun.ts.tests.common.vehicle")) {
         //    return true;
