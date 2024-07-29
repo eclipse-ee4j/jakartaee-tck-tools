948ea5b7ea6aa660102f42f44429461aa51a0f81
Scott M Stark <starksm64@gmail.com>
2024-07-28 20:42:07 -0600

    Fix some issues with descriptor paths Use common protocol.jar for appclient/javatest classes
    Signed-off-by: Scott M Stark <starksm64@gmail.com>

--- arquillian/javatest/src/main/java/tck/arquillian/protocol/javatest/JavaTestDeploymentPackager.java
+++ arquillian/javatest/src/main/java/tck/arquillian/protocol/javatest/JavaTestDeploymentPackager.java
@@ -4,9 +4,11 @@ import org.jboss.arquillian.container.test.spi.TestDeployment;
 import org.jboss.arquillian.container.test.spi.client.deployment.DeploymentPackager;
 import org.jboss.arquillian.container.test.spi.client.deployment.ProtocolArchiveProcessor;
 import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.EnterpriseArchive;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 
+import java.io.File;
 import java.util.Collection;
 import java.util.logging.Logger;
 
@@ -18,12 +20,16 @@ public class JavaTestDeploymentPackager implements DeploymentPackager {
         Archive<?> archive = testDeployment.getApplicationArchive();
 
         Collection<Archive<?>> auxiliaryArchives = testDeployment.getAuxiliaryArchives();
+        File protocolJar = new File("target/protocol/protocol.jar");
+
         if(archive instanceof EnterpriseArchive) {
             EnterpriseArchive ear = (EnterpriseArchive) archive;
             ear.addAsLibraries(auxiliaryArchives.toArray(new Archive<?>[0]));
+            ear.addAsLibraries(protocolJar);
         } else if(archive instanceof WebArchive) {
             WebArchive war = (WebArchive) archive;
             war.addAsLibraries(auxiliaryArchives.toArray(new Archive<?>[0]));
+            war.addAsLibraries(protocolJar);
         }
 
         return archive;
